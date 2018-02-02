a0a2c03eb66161c86aad2b7005e335162d360543
Serge Semin <fancer.lancer@gmail.com>
2018-02-02 22:53:03 +0300

    baikal: Add cmd_exit for QEMU
    We need to be able to exit from QEMU emulation process even in the
    framework of U-boot process. This cmd can be used exactly for this.
    
    Original code is taken from BaikalSDK 4.14
    
    Signed-off-by: Serge Semin <fancer.lancer@gmail.com>

---
+++ board/baikal/mips/exit.c
@@ -0,0 +1,35 @@
+/* 
+ * File:        exit.c 
+ * Description: Implementation of proprenitary 'exit' command
+ * Created:     15 Nov 2013
+ * Author:      Dmitry Dunaev <dmitry.dunaev@baikalelectronics.ru>
+ * Copyright 2013 Baikal Electronics
+ *
+ */
+
+#include <common.h>
+#include <command.h>
+#include <asm/io.h>
+
+#ifdef CONFIG_CMD_EXIT
+
+#define QEMU_EXIT_BLOCK KSEG1ADDR(0x1F0FF000)
+
+static int do_exit(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	if (argc != 2)
+		return CMD_RET_USAGE;
+
+    __raw_writel(simple_strtoul(argv[1], NULL, 10), QEMU_EXIT_BLOCK);
+    return 0;
+}
+
+U_BOOT_CMD(
+    exit ,    2,    1,     do_exit,
+    "exit from simulator with specified error code",
+    "RC\n"
+    "    - exit with exit code = RC (RC is _decimal_ !!!)"
+);
+#endif
+/* End of file exit.c */
+
