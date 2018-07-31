7f3fde3ecafebe8a23266d73745476b2444f72ee
Serge Semin <fancer.lancer@gmail.com>
2018-07-31 05:00:17 +0300

    baikal: Add T-platforms Axitech BC3BT1 MRBT1 board initialization
    This device is in fact the carrier board for mezzanine MRBT1 module
    Even though the actual device got SSD, 3G-modem and a plenty of
    rs232/rs485 ftdi/cp2108-based port, U-boot code supports just basic
    functionality, including the reset GPIO-expanders installed on the
    board. Mezzanine module is selected by U-boot default configuration
    file. Root board_early_init_r and board_late_init methods are also defined
    empty for now.
    
    Signed-off-by: Serge Semin <fancer.lancer@gmail.com>

---
+++ include/configs/tplatforms_bc3bt1.h
@@ -0,0 +1,62 @@
+/*
+ * Configuration settings for Axitech BC3BT1 with MRBT1 boards installed.
+ *
+ * Copyright (C) 2018 T-platforms JSC
+ *
+ * Authors:
+ *   Segey Semin <sergey.semin@t-platforms.ru>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef __CONFIG_TPLATFORMS_BC3BT1_H
+#define __CONFIG_TPLATFORMS_BC3BT1_H
+
+#include <generated/autoconf.h>
+
+#include <configs/tplatforms_mrbt1.h>
+
+/*
+ *-----------------------------------------------
+ * Board common settings
+ *-----------------------------------------------
+ */
+#undef CONFIG_BAIKAL_NAME
+#define CONFIG_BAIKAL_NAME		"BC3BT1-MRBT1"
+
+/*
+ *-----------------------------------------------
+ * Axitech board specific hardware
+ *-----------------------------------------------
+ */
+#define CONFIG_SYS_CTRL_BUS_NUM		0
+#define CONFIG_SYS_CTRL_I2C_ADDR	0x22
+#define CONFIG_SYS_LEDS_BUS_NUM		0
+#define CONFIG_SYS_LEDS_I2C_ADDR	0x23
+#define CONFIG_SYS_I2C_PCA953X_WIDTH \
+	{{CONFIG_SYS_CTRL_I2C_ADDR, 16, CONFIG_SYS_CTRL_BUS_NUM}, \
+	 {CONFIG_SYS_LEDS_I2C_ADDR, 16, CONFIG_SYS_LEDS_BUS_NUM}}
+#define CONFIG_SYS_CTRL_SD_RST_PIN	4
+#define CONFIG_SYS_CTRL_SSD_RST_PIN	5
+#define CONFIG_SYS_CTRL_GSM_RST_PIN	6
+#define CONFIG_SYS_CTRL_FTDI_RST_PIN	9
+#define CONFIG_SYS_CTRL_USBHUB_RST_PIN	11
+
+/*
+ *-----------------------------------------------
+ * Ethernet controller parameters
+ *-----------------------------------------------
+ */
+#define CONFIG_MSCC_LEDS_ACTIVE_HIGH
+#define CONFIG_MSCC_LEDS_DRIVE_LOW_HIGH
+
+/*
+ *-----------------------------------------------
+ * Board RTC parameters
+ *-----------------------------------------------
+ */
+#define CONFIG_CMD_DATE
+#define CONFIG_RTC_PCF2127
+#define CONFIG_SYS_RTC_BUS_NUM  0x01
+#define CONFIG_SYS_I2C_RTC_ADDR 0x51
+
+#endif /* __CONFIG_TPLATFORMS_BC3BT1_H */
