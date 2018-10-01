3324077177ced4278d05334426eb8f1026d0f861
Serge Semin <fancer.lancer@gmail.com>
2018-10-01 09:30:16 +0300

    baikal: Add T-platforms RT1MB board initialization code
    This commit provides the initialization code for RT1MB boards.
    It doesn't provide any much of initialization, since the board
    doesn't have any control expanders or easily accessed eeproms.
    For now we just have a simple SoC init code with basic
    peripherals setups.
    
    Signed-off-by: Serge Semin <fancer.lancer@gmail.com>

---
+++ board/baikal/mips/rt1mb/board.c
@@ -0,0 +1,51 @@
+/*
+ * File:        board.c
+ * Description: T-platforms MRBT1 Baikal-T1 based board initialization
+ * Authors:     Sergey Semin <sergey.semin@t-platforms.ru>
+ *
+ * Copyright 2018 T-platforms JSC
+ */
+
+#include <common.h>
+#include <asm/io.h>
+#include <miiphy.h>
+#include <netdev.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#ifdef CONFIG_BOARD_EARLY_INIT_R
+int board_early_init_r(void)
+{
+	return 0;
+}
+#endif /* CONFIG_BOARD_EARLY_INIT_R */
+
+#ifdef CONFIG_BOARD_LATE_INIT
+int board_late_init(void)
+{
+	return 0;
+}
+#endif /* CONFIG_BOARD_LATE_INIT */
+
+/* Initialization of network */
+int board_eth_init(bd_t *bis)
+{
+	int err = 0;
+
+#if defined(CONFIG_DESIGNWARE_ETH0_BASE)
+	if (designware_initialize(CONFIG_DESIGNWARE_ETH0_BASE,
+			  PHY_INTERFACE_MODE_RGMII) < 0)
+		err |= (1 << 0);
+#endif /* CONFIG_DESIGNWARE_ETH0_BASE */
+#if defined(CONFIG_DESIGNWARE_ETH1_BASE)
+	if (designware_initialize(CONFIG_DESIGNWARE_ETH1_BASE,
+			  PHY_INTERFACE_MODE_RGMII) < 0)
+		err |= (1 << 1);
+#endif /* CONFIG_DESIGNWARE_ETH1_BASE */
+#if defined(CONFIG_DESIGNWARE_ETH2_BASE)
+	if (designware_initialize(CONFIG_DESIGNWARE_ETH2_BASE,
+			  PHY_INTERFACE_MODE_RGMII) < 0)
+		err |= (1 << 1);
+#endif /* CONFIG_DESIGNWARE_ETH2_BASE */
+	return (! err);
+}
