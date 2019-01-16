96837fb94b0b0aef1814ab38eff922f6809f08e5
Vasily Yurchenko <vasiliy.yurchenko@t-platforms.ru>
2019-01-16 17:10:51 +0300

    Debugging functions were fixed. prepare_mmu function was fixed.

--- arch/mips/include/asm/dbgout.h
+++ arch/mips/include/asm/dbgout.h
@@ -10,27 +10,45 @@ void ddr_ddrc_regs_print(void);
 // Place here definitions for asm-code
 
 .macro	dbg_out type, val
-	
+
+	_savereg stack
 	lui a0, (\type >> 16)
 	ori a0, a0,(\type & 0x0000FFFF)
 	add a1, zero, \val
-	bal debug_out
+
+	la t9, debug_out
+	jalr t9
+	nop
+	_restorereg stack
 .endm
 
 
+.macro	dbg_out_val val
+
+	_savereg stack
+	lui a0, (0xDEAD)
+	ori a0, (0x0010)
+	lui a1, (\val >> 16)
+	ori a1, (\val & 0xFFFF)
+
+	la t9, debug_out
+	jalr t9
+	nop
+	_restorereg stack
+.endm
 
 #endif // ASM
 // Place here common definitions
 
 #define TYPEMAGIC	0xDEAD0000
 #define TYPEMASK	0xFFFF0000
-#define TREG		0xDEADFFFF
-#define CFG0		0xDEAD0CF0
-#define CFG1		0xDEAD0CF1
-#define CFG2		0xDEAD0CF2
-#define CFG3		0xDEAD0CF3
-#define CFG4		0xDEAD0CF4
-#define CFG5		0xDEAD0CF5
-#define CFG6		0xDEAD0CF6
-
+#define TREG		0xDEADFFF0
+#define CFG0		0xDEADCF00
+#define CFG1		0xDEADCF10
+#define CFG2		0xDEADCF20
+#define CFG3		0xDEADCF30
+#define CFG4		0xDEADCF40
+#define CFG5		0xDEADCF50
+#define CFG6		0xDEADCF60
+#define VAL		0xDEAD0010
 #endif // DEBUG_OUT
--- common/dbgout.c
+++ common/dbgout.c
@@ -1,15 +1,23 @@
 #include <common.h>
 #include <asm/dbgout.h>
 
-void debug_out(int type, int val) {
-	printf("DBG: %s()\n", __func__);
+void debug_out(unsigned int type, unsigned int val) {
+	int arr[4] = {0x12345678, 0xA55A5AA5, 0xBEAFDEAD, 0xBABADEDA};
+//	printf("DBG: arr address 0x%08X\n", arr);
+//	printf("DBG: %s()\n", __func__);
+//	printf("DBG: type: 0x%08X, TYPEMASK: 0x%08X, TYPEMAGIC: 0x%08X\n", type, TYPEMASK, TYPEMAGIC);
+//	printf("DBG: type & TYPEMASK: 0x%08X\n", type & TYPEMASK);
+//	printf("DBG: type & TYPEMASK != TYPEMAGIC: %d\n", type & TYPEMASK != TYPEMAGIC);
 	if(type & TYPEMASK != TYPEMAGIC) {
 		printf("DBG_OUT: Type:0x%X, Value:0x%X\n", type, val);
 		return;
 	}
-
+	if(type == VAL) {
+		printf("DBG_OUT: Value:0x%X\n", val);
+		return;
+	}
 	if(type == TREG) {
-		printf("DBG_OUT: t register value:0x%X\n", val);
+		printf("DBG_OUT: Register:0x%X\n", val);
 		return;
 	}
 	if(type == CFG0) {
