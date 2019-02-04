a407516a2101ce221610fc8b97949a07a9cd9711
Serge Semin <fancer.lancer@gmail.com>
2019-02-05 00:25:40 +0300

    baikal: Move MAC/Serial-specific init to FRU library
    Aside from just ordinary FRU blob parsing there are two more
    things, which are done with retrieved data. It is a MAC-addresses
    and board serial numbers. Since all of these actions are performed
    for FRU-available boards lets move it to the FRU module to simplify
    the boards-specific code.
    
    Signed-off-by: Serge Semin <fancer.lancer@gmail.com>

--- board/baikal/mips/bn1bt1/board.c
+++ board/baikal/mips/bn1bt1/board.c
@@ -18,33 +18,6 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-/* Set enviroment variables of all mac address from EEPROM. */
-static void __maybe_unused setall_macaddr(void)
-{
-    uchar enetaddr[6];
-    char name[32];
-    int i;
-
-    for (i = 0; i < CONFIG_FRU_MAX_IFACES; ++i) {
-        switch (i) {
-        case 0:
-            memcpy(enetaddr, fru.mac0, 6);
-            break;
-        case 1:
-            memcpy(enetaddr, fru.mac1, 6);
-            break;
-        case 2:
-            memcpy(enetaddr, fru.mac2, 6);
-            break;
-        default:
-            break;
-        }
-
-        sprintf(name, i ? "eth%daddr" : "ethaddr", i);
-        eth_setenv_enetaddr(name, enetaddr);
-    }
-}
-
 #ifdef CONFIG_BOARD_EARLY_INIT_R
 int board_early_init_r(void)
 {
@@ -54,11 +27,7 @@ int board_early_init_r(void)
     int start = 0;
     int gpio_usb_reset = 13;
 
-    memset(fru.bootdevice, 0, FRU_STR_MAX);
     fru_open_parse();
-
-    printf("P/N:   %s\n", fru.val_part_number);
-    printf("S/N:   %s\n", fru.val_serial_number);
 
     /* reset USB hub and configure it */
     debug("Reset and configure USB hub: ");
@@ -123,13 +92,6 @@ int board_late_init(void)
             printf("FRU: unknown boot device %s, falling back to %s\n", fru.bootdevice, disk1);
         }
     }
-
-    /* Set mac addresses */
-    setall_macaddr();
-
-    setenv("board_serial", (char *)fru.val_serial_number);
-    setenv("board_rev", (char *)fru.val_part_number);
-    setenv("board_name", (char *)fru.val_product_name);
 
 #ifdef CONFIG_SATA_PHY_INIT
     unsigned int phy = getenv_ulong("sataphy", 10, 0);
