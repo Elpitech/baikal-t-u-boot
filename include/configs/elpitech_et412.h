83c9a93d5271ae7639618bf5a53ff5ddb2ac4a2c
Tanya <Tatiyana.Kopytsa@elpitech.ru>
2022-05-05 14:05:23 +0300

    Add Elpitech et412 support

---
+++ include/configs/elpitech_et412.h
@@ -0,0 +1,26 @@
+/*
+ * Configuration settings for the Baikal-T1 based ET412 boards.
+ *
+ * Copyright (C) 2022 Elpitech
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef __CONFIG_ELPITECH_ET412_H
+#define __CONFIG_ELPITECH_ET412_H
+
+/*
+ *-----------------------------------------------
+ * Default Baikal-T1 config macro
+ *-----------------------------------------------
+ */
+#include <configs/tplatforms_mitx.h>
+
+/*
+ *-----------------------------------------------
+ * Generic Configurations
+ *-----------------------------------------------
+ */
+#undef  BOARD_NAME
+#define BOARD_NAME			"ET412"
+
+#endif /* __CONFIG_ELPITECH_ET412_H */
